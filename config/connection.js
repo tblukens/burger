273291dd7db491f62ff783fd8cba426dbd41871c
Tim Lukens <tblukens87@gmail.com>
2018-11-11 10:54:30 -0600

    Setup config/connection.js for heroku jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+let connection;
 
-var connection = mysql.createConnection({
-  host: process.env.MYDB_HOST,
-  port: process.env.MYDB_PORT || 3306,
-  user: process.env.MYDB_USER,
-  password: process.env.MYDB_PASS,
-  database: process.env.MYDB_NAME
-});
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: process.env.MYDB_HOST,
+    port: process.env.MYDB_PORT || 3306,
+    user: process.env.MYDB_USER,
+    password: process.env.MYDB_PASS,
+    database: process.env.MYDB_NAME
+  });
+};
 
 // Make connection.
 connection.connect(function(err) {
